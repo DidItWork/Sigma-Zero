a8a66bd7eaa907276abc816b151877c3fa634740
s-hin-kansen <shaun_hin@mymail.sutd.edu.sg>
2024-03-17 15:52:24 +0800

    Update trees.py

--- trees.py
+++ trees.py
@@ -3,6 +3,7 @@ from torch import tensor
 from __future__ import annotations
 import chess
 import numpy as np
+import random
 
 """
 Stores trees of different games
@@ -47,11 +48,16 @@ class Node(ABC):
         self.W = 0.0
         self.N = 0
         self.parent = p
+        self.children = []
+        self.explored_moves = []
 
         # Add the number of next moves possible
         self.policy = []
-        for move in self.state.legal_moves:
-            self.policy.append(Node(self.state.push(move), self))
+        # for move in self.state.legal_moves:
+        #     self.policy.append(Node(self.state.push(move), self))
+
+        self.visit_count = 0
+        self.value_sum = 0.0
 
     @abstractmethod
     def getNextState(self) -> Node:
@@ -60,7 +66,6 @@ class Node(ABC):
         that has not already been explored and update children list with new state
 
         """
-
         pass
 
     @abstractmethod
@@ -71,6 +76,14 @@ class Node(ABC):
 
         self.W += W
         self.N += 1
+
+    @abstractmethod
+    def updateValue(self, value: float = 0.0) -> None:
+        """
+        Update policy value (default 0.0) during backpropagation
+        """
+
+        self.value_sum += value
 
     @abstractmethod
     def __get__(self) -> tuple:
@@ -90,6 +103,8 @@ class Node(ABC):
         """
 
         # TODO: How do you determine this? Is a node ever fully explored?
+        # Node fully explored if all possible moves at 1 depth are exhausted? children == policy?
+        return len(self.children) == len(self.policy)
         pass
 
     @abstractmethod
@@ -99,3 +114,66 @@ class Node(ABC):
         """
 
         return len(self.policy) == 0
+
+
+class ChessNode(Node):
+
+    def __init__(self, s=None, p: Node = None) -> None:
+        super().__init__()
+
+    def getNextState(self) -> Node:
+        """
+        Returns a node with a random new state that is reachable from the current state
+        that has not already been explored and update children list with new state
+
+        """
+        unexplored_moves = [x for x in self.state.legal_moves if x not in self.explored_moves]
+        explored_move = random.choice(unexplored_moves)
+        # TODO: are explored Nodes propagated and backpropped as well to be in the state?
+        self.explored_moves.append(explored_move)
+        child = ChessNode(self.state.push(explored_move), self)
+        self.chilren.append(child)
+        return child
+        pass
+
+    def updateWN(self, W: float = 0.0) -> None:
+        """
+        Update "Wins" and number of simulations (by default 1) during backpropagation
+        """
+
+        self.W += W
+        self.N += 1
+
+    def updateValue(self, value: float = 0.0) -> None:
+        """
+        Update policy value (default 0.0) during backpropagation
+        """
+
+        self.value_sum += value
+
+    def __get__(self) -> tuple:
+        """
+        Returns a tuple of node attributes needed to generate training set,
+        e.g. state and policy (action taken)
+        """
+
+        # FIXME: What kind of tuple you expect here?
+
+        return (self.state, self.policy)
+
+    def explored(self) -> bool:
+        """
+        Returns if the node has been fully explored, no more children to be generated from getNextState
+        """
+
+        # TODO: How do you determine this? Is a node ever fully explored?
+        # Node fully explored if all possible moves at 1 depth are exhausted? children == legal_moves?
+        return len(self.children) == len(self.state.legal_moves)
+
+    def terminal(self) -> bool:
+        """
+        Returns if the node is a terminal node (no children)
+        """
+
+        return len(self.policy) == 0
+    
