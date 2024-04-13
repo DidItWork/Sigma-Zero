63606126694475fcd97e50a9764522131af696b1
spizzray <spizzray@gmail.com>
2024-04-13 12:39:04 +0800

    UI chanegs after meeting for home page and history

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -7,6 +7,8 @@ import time
 # TODO When 'start' button is pressed, instantiate a new game, go to 'Play' page
 
 st.header("SigmaZero")
+st.markdown('Try to beat SigmaZero in a game of chess!')
+st.markdown('You can try playing vanilla chess, where your usual strategies can be applied, or challenge yourself with Chess960, where the starting positions of the pieces are randomized.')
 st.markdown('---')
 
 # Define game options
@@ -15,31 +17,34 @@ mode = st.selectbox(
     ('Vanilla Chess', 'Chess960')
 )
 
-black_player = st.selectbox(
-    'Black',
-    ('Human', 'SigmaZero', 'StockFish')
+player_color = st.selectbox(
+    'Which color would you like to play as?',
+    ('White', 'Black')
 )
 
-white_player = st.selectbox(
-    'White',
-    ('Human', 'SigmaZero', 'StockFish')
-)
+# number_of_games = st.selectbox(
+#     'Number of Games',
+#     ('1', '3', '5', '10', '20', '50', '100')
+# )
 
-number_of_games = st.selectbox(
-    'Number of Games',
-    ('1', '3', '5', '10', '20', '50', '100')
-)
+if player_color == 'White':
+    white_player = 'Human'
+    black_player = 'SigmaZero'
+else:
+    white_player = 'SigmaZero'
+    black_player = 'Human'
 
 # Action to perform when 'Start' button is clicked
 if st.button('Start'):
     # Store game settings in a session state to preserve them across reruns
     st.session_state['game_mode'] = mode
-    st.session_state['black_player'] = black_player
+    st.session_state['player_color'] = player_color
     st.session_state['white_player'] = white_player
-    st.session_state['number_of_games'] = int(number_of_games)
+    st.session_state['black_player'] = black_player
+    # st.session_state['number_of_games'] = int(number_of_games)
 
     # display a success message
-    st.success(f"Game is starting with {white_player} (White) vs {black_player} (Black) in {mode} mode for {number_of_games} game(s).")
+    st.success(f"Game is starting with {white_player} (White) vs {black_player} (Black) in {mode} mode!")
 
     ######AAABBRAAAAMMMMMMMMMMM... HELP REPLACE THIS ONE !!!!!!!!!!!!!!!!!!!!!!!!
     # Placeholder for actual game initialization #ABRAM PLS REPLACE THIS WITH ACTUAL GAME INITIALIZATION
@@ -54,4 +59,4 @@ if 'game_mode' in st.session_state:
     st.write(f"Mode: {st.session_state['game_mode']}")
     st.write(f"Black: {st.session_state['black_player']}")
     st.write(f"White: {st.session_state['white_player']}")
-    st.write(f"Number of Games: {st.session_state['number_of_games']}")
+    # st.write(f"Number of Games: {st.session_state['number_of_games']}")
