4f30ee797fcd1eb5806c0e1ed90061d627e6e573
s-hin-kansen <shaun_hin@mymail.sutd.edu.sg>
2024-04-14 14:45:32 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -33,89 +33,97 @@ def play_game(model, args):
 
     # modified settings from Lichess fishnet distribution skill levels
     # level_num: (skill_level, time_limit, depth_limit)  
-    # 2: (3, 0.02, 5) lost to this initial setting roughly 1.5k elo
+    # 2: (3, 0.02, 5) old model lost to this initial setting roughly 1.5k elo
     # 
-    level = {1: (0, 0.01, 1),
-             2: (1, 0.02, 2),
-             3: (2, 0.03, 3),
-             4: (3, 0.04, 4),
-             5: (4, 0.05, 5),
-             6: (5, 0.06, 6),
-             7: (6, 0.07, 7),
+    level = {1: (0, 1, 3),
+             2: (1, 1, 3),
+             3: (2, 1, 3),
+             4: (3, 1, 4),
+             5: (4, 1, 5),
+             6: (5, 1, 6),
+             7: (6, 2, 7),
              8: (20, 10, 50)}
 
     level_reached = 1
+    model_score = 0
     while True:
-        board = chess.Board()
-        chess_tensor = ChessTensor()
+        for _ in range(5):  # 5 matches
+            board = chess.Board()
+            chess_tensor = ChessTensor()
 
-        stockfish_path = "./stockfish/stockfish-windows-x86-64-avx2.exe"
-        engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
+            stockfish_path = "./stockfish/stockfish-windows-x86-64-avx2.exe"
+            engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
 
-        skill_level = level[level_reached][0]
-        time_limit = level[level_reached][1]
-        depth_limit = level[level_reached][2]      
-        
-        engine.configure({"Skill Level": skill_level})
- 
-        while not board.is_game_over():
+            skill_level = level[level_reached][0]
+            time_limit = level[level_reached][1]
+            depth_limit = level[level_reached][2]      
+            
+            engine.configure({"Skill Level": skill_level})
+    
+            while not board.is_game_over():
 
+                print(board)
+
+                if board.turn:
+
+                    # move = input("Please enter move: ")
+
+                    # best_move = chess.Move.from_uci(move.strip())
+
+                    limit = chess.engine.Limit(time=time_limit, depth=depth_limit)
+
+                    result = engine.play(board, limit)
+
+                    best_move = result.move
+
+                    time.sleep(1)
+                
+                else:
+
+                    t1 = time.perf_counter()
+
+                    mcts = MCTS0(game=chess_tensor, model=model, args=args)  # Create a new MCTS object for every turn
+                
+                    action_probs = mcts.search(board, verbose=False, learning=False)
+
+                    print(action_probs)
+
+                    t2 = time.perf_counter()
+
+                    best_move = max(action_probs, key=action_probs.get)
+
+                    print("Search Time", t2-t1)
+
+                print("Board", chess_tensor.board.turn)
+
+                board.push(best_move)
+                
+                # Update the chess tensor with the new move
+                chess_tensor.move_piece(best_move)
+
+                
+                #debug code
+                # print(f"Init time: {t2-t1}\nSearch time: {t3-t2}\nMove time: {t4-t3}\nLoop time: {t4-t1}")
+                
             print(board)
 
-            if board.turn:
+            # Determine the game result and assign rewards
+            result = board.result()
+            print("Result", result)
 
-                # move = input("Please enter move: ")
+            if result == "0-1":
+                model_score += 1
+            elif result == "1/2-1/2":
+                model_score += 0.5
 
-                # best_move = chess.Move.from_uci(move.strip())
+            if model_score > 5:  # model won 5 points on stockfish, proceed to next level
+                break
 
-                limit = chess.engine.Limit(time=time_limit, depth=depth_limit)
-
-                result = engine.play(board, limit)
-
-                best_move = result.move
-
-                time.sleep(1)
-            
-            else:
-
-                t1 = time.perf_counter()
-
-                mcts = MCTS0(game=chess_tensor, model=model, args=args)  # Create a new MCTS object for every turn
-            
-                action_probs = mcts.search(board, verbose=False, learning=False)
-
-                print(action_probs)
-
-                t2 = time.perf_counter()
-
-                best_move = max(action_probs, key=action_probs.get)
-
-                print("Search Time", t2-t1)
-
-            print("Board", chess_tensor.board.turn)
-
-            board.push(best_move)
-            
-            # Update the chess tensor with the new move
-            chess_tensor.move_piece(best_move)
-
-            
-            #debug code
-            # print(f"Init time: {t2-t1}\nSearch time: {t3-t2}\nMove time: {t4-t3}\nLoop time: {t4-t1}")
-            
-        print(board)
-
-        # Determine the game result and assign rewards
-        result = board.result()
-        print("Result", result)
-
-        if result == "1-0":
+        if model_score <= 5:
             break
-        elif result == "1/2-1/2":
-            engine.quit()
         else:
             level_reached += 1
-            engine.quit()
+
     engine.quit()
 
     return level_reached
@@ -138,7 +146,7 @@ if __name__ == "__main__":
 
     ct = ChessTensor()
 
-    model.load_state_dict(model_weights, strict=False)
+    model.load_state_dict(model_weights)
     
     level_reached = play_game(model=model, args=args)
 
